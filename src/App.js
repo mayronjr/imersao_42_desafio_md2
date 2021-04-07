d818f540bc76e416c8c73f8cf612a525f9323b44
Mayron Júnior <mayronjunior5@ufpi.edu.br>
2021-04-07 03:08:16 -0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        First Commit
+      </header>
+      <div>
+        First Commit
+      </div>
+    </div>
+  );
+}
+
+export default App;
